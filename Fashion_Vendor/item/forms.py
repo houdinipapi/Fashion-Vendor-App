c73b92920ae9ec17285fed07ae5337bccac6126e
Houdini Papi <ochiengcliff.co@gmail.com>
2024-01-29 22:23:22 +0300

    Add button-type and axe/forms to .hintrc, update Browse link in base.html, add items.html template, and add edit view in views.py

--- Fashion_Vendor/item/forms.py
+++ Fashion_Vendor/item/forms.py
@@ -4,15 +4,39 @@ from .models import Item
 
 INPUT_CLASSES = "w-full py-4 px-6 rounded-xl border border-gray-300 bg-gray-100 focus:bg-white focus:outline-none"
 
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ("category", "name", "description", "price", "image",)
+        fields = ("name", "description", "price", "category", "image")
 
         widgets = {
+            "name": forms.TextInput(attrs={
+                "class": INPUT_CLASSES,
+                "placeholder": "Item name",
+            }),
+            "description": forms.Textarea(attrs={
+                "class": INPUT_CLASSES,
+                "placeholder": "Item description",
+            }),
+            "price": forms.TextInput(attrs={
+                "class": INPUT_CLASSES,
+                "placeholder": "Item price",
+            }),
             "category": forms.Select(attrs={
                 "class": INPUT_CLASSES,
             }),
+            "image": forms.FileInput(attrs={
+                "class": INPUT_CLASSES,
+            }),
+        }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ("name", "description", "price", "image", "is_sold")
+
+        widgets = {
             "name": forms.TextInput(attrs={
                 "class": INPUT_CLASSES,
                 "placeholder": "Item name",
--- Fashion_Vendor/item/views.py
+++ Fashion_Vendor/item/views.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
 
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 from .models import Item
 
 
 # Create your views here.
+def items(request):
+    query = request.GET.get("query", "")
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, "item/items.html", {
+        "items": items,
+        "query": query,
+    })
+
 def detail(request, pk):
     item = get_object_or_404(
         Item, pk=pk
@@ -41,6 +50,30 @@ def new(request):
         {
             "form": form,
             "title": "New Item",
+        },
+    )
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == "POST":
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("item:detail", pk=item.pk)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(
+        request,
+        "item/form.html",
+        {
+            "form": form,
+            "title": "Edit Item",
         },
     )
 
