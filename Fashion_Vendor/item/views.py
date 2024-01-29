019e0bb3ddfb389220bb695e1617a922286cfbf6
Houdini Papi <ochiengcliff.co@gmail.com>
2024-01-29 23:45:51 +0300

    Update search button and add clear filters option

--- Fashion_Vendor/item/views.py
+++ Fashion_Vendor/item/views.py
@@ -9,9 +9,12 @@ from .models import Category, Item
 # Create your views here.
 def items(request):
     query = request.GET.get("query", "")
+    category_id = request.GET.get("category", 0)
     categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
 
+    if category_id:
+        items = items.filter(category_id=category_id)
 
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
@@ -20,6 +23,7 @@ def items(request):
         "items": items,
         "query": query,
         "categories": categories,
+        "category_id": int(category_id),
     })
 
 def detail(request, pk):
